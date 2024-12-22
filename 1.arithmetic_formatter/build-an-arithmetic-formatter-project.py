0a896b4a915bee71ca5b94b18e6bcfc32282c81d
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2024-12-22 01:52:37 +0000

    arithmetic formatter

---
+++ 1.arithmetic_formatter/build-an-arithmetic-formatter-project.py
@@ -0,0 +1,45 @@
+def arithmetic_arranger(problems, show_answers=False):
+    if len(problems) > 5: 
+        return "Error: Too many problems."
+        #raise ValueError("Error: Too many problems.")
+    
+    first_line = ''
+    second_line = ''
+    dash_line = ''
+    answer_line = ''
+    blank_space = '    '
+
+    for problem in problems: 
+        first_operand, operator, second_operand = problem.split()
+
+        if not (operator == '+' or operator == '-'):
+            return "Error: Operator must be '+' or '-'."
+            #raise ValueError("Error: Operator must be '+' or '-'.")
+
+        if not (first_operand.isdigit() and second_operand.isdigit()): 
+            return "Error: Numbers must only contain digits."
+            #raise ValueError("Error: Numbers must only contain digits.")
+        if len(first_operand) > 4 or len(second_operand) > 4:
+            return "Error: Numbers cannot be more than four digits."
+            #raise ValueError("Error: Numbers cannot be more than four digits")
+
+        if show_answers:
+            answer = str(eval(problem))
+
+        max_len = max(len(first_operand), len(second_operand)) + 2
+        first_line += first_operand.rjust(max_len) + blank_space
+        second_line += operator + ' ' + second_operand.rjust(max_len - 2) + blank_space
+        dash_line += '-' * max_len + blank_space
+        if show_answers:
+            answer_line += answer.rjust(max_len) + blank_space
+
+        arranged_problems = f"{first_line.rstrip()}\n{second_line.rstrip()}\n{dash_line.rstrip()}"  
+
+    if show_answers:
+        arranged_problems += f"\n{answer_line.rstrip()}"
+       
+    problems = arranged_problems
+    return problems
+
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"],True)}')
